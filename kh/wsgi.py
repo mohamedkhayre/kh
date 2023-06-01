af1fe74294675a483063170d03417897c4418d1a
mohamedkhayre <135255146+mohamedkhayre@users.noreply.github.com>
2023-06-01 22:28:02 +0300

    Update wsgi.py

--- kh/wsgi.py
+++ kh/wsgi.py
@@ -1,20 +1,16 @@
-# This file contains the WSGI configuration required to serve up your
-# web application at http://khh.pythonanywhere.com/
-# It works by setting the variable 'application' to a WSGI handler of some
-# description.
-#
-# The below has been auto-generated for your Django project
+"""
+WSGI config for kh project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/khh/mysite'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-## Uncomment the lines below depending on your Django version
-###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kh.settings')
+
 application = get_wsgi_application()
