7116b5162956ed204487f21020709301eebb93b5
mohamedkhayre <135255146+mohamedkhayre@users.noreply.github.com>
2023-06-01 22:33:24 +0300

    khh.pythonanywhere.com

--- kh/wsgi.py
+++ kh/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for kh project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, seeb
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
